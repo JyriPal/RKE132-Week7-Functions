43bdb75337f238157269d79c41dfd08bf4594cb8
Magic Clip <magic@clip.com>
2024-04-23 10:05:28 +0300

    add Week 7 code

---
+++ Functions/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Channels;
+
+Console.WriteLine("Are you coming or leaving? (in/out):");
+string userChoice = Console.ReadLine();
+if(userChoice == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodbye();
+}
+
+
+
+
+
+
+
+static void PrintHello() //functions / method
+{
+    Console.WriteLine("Hello, World!");
+}
+
+static void PrintGoodbye() 
+{
+    Console.WriteLine("See you later, aligator.");
+}
+
